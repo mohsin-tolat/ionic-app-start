72d08759608169000b988abf9ade869eaefef82e
Mohsin Tolat <tolatm@pmc.local>
2020-01-09 12:06:04 +0530

    Changes related to Storing the images

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+import { Storage } from '@ionic/storage';
 
 @Injectable({
   providedIn: 'root',
@@ -7,7 +8,7 @@ import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 export class PhotoService {
   public photos: Photo[] = [];
 
-  constructor(private camera: Camera) {}
+  constructor(private camera: Camera, private storage: Storage) {}
 
   takePicture() {
     const options: CameraOptions = {
@@ -23,12 +24,20 @@ export class PhotoService {
         this.photos.unshift({
           data: 'data:image/jpeg;base64,' + imageData,
         });
+
+        this.storage.set('photos', this.photos);
       },
       err => {
         // Handle error
         console.log('Camera issue: ' + err);
       }
     );
+  }
+
+  loadSaved() {
+    this.storage.get('photos').then(photos => {
+      this.photos = photos || [];
+    });
   }
 }
 
