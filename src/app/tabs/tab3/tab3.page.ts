223ec12f0a17e20ead75ffc8942e00f8b31fbd35
Mohsin Tolat <tolatm@pmc.local>
2020-01-21 18:11:08 +0530

    Clear local storage when logged out

--- src/app/tabs/tab3/tab3.page.ts
+++ src/app/tabs/tab3/tab3.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { LocalStorageService } from 'ngx-webstorage';
 
 @Component({
   selector: 'app-tab3',
@@ -7,9 +8,13 @@ import { Router } from '@angular/router';
   styleUrls: ['tab3.page.scss'],
 })
 export class Tab3Page {
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private localStorageService: LocalStorageService
+  ) {}
 
   logout() {
+    this.localStorageService.clear();
     this.router.navigateByUrl('/');
   }
 }
